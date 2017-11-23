66c93731dc9e496508458268116737c9a798b507
Alexis Leclerc <pihaelle@gmail.com>
2017-11-23 15:14:33 +0100

    [VCR] Update cassettes for homepage

--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -4,6 +4,7 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.configure_rspec_metadata!
   c.default_cassette_options = { allow_playback_repeats: true }
+  # c.default_cassette_options = { allow_playback_repeats: true, record: :new_episodes }
 
   c.filter_sensitive_data('<APIE_TOKEN>') { Rails.application.config_for(:secrets) ['apie_token'].to_s }
   c.filter_sensitive_data('<APIE_BASE_URI>') { Rails.application.config_for(:secrets) ['apie_base_uri'].to_s }
