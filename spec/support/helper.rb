86814e497de7beb394737f3377e7afc54cb5d132
Alexis Leclerc <pihaelle@gmail.com>
2017-11-29 11:43:26 +0100

    remove spec helper function

--- spec/support/helper.rb
+++ spec/support/helper.rb
@@ -19,25 +19,3 @@ class FakeWorker
     end
   end
 end
-
-def capture_output
-  old_stdout = STDOUT.clone
-  pipe_r, pipe_w = IO.pipe
-  pipe_r.sync    = true
-  output         = ""
-  reader = Thread.new do
-    begin
-      loop do
-        output << pipe_r.readpartial(1024)
-      end
-    rescue EOFError
-    end
-  end
-  STDOUT.reopen(pipe_w)
-  yield
-ensure
-  STDOUT.reopen(old_stdout)
-  pipe_w.close
-  reader.join
-  return output
-end
