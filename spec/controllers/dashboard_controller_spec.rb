8374d5cc9c7b83936d7dc7347bac574652b17218
Alexis Leclerc <pihaelle@gmail.com>
2017-12-04 08:41:03 +0100

    [Rspec] add json-schema

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -8,15 +8,8 @@ describe DashboardController, type: :controller do
       expect(subject.status).to eq(200)
     end
 
-    it 'returns well formated json' do
-      json = JSON.parse(subject.body)
-      expect(json).to be_a(Hash)
-
-      json['results'].each do |e|
-        expect(e['name']).not_to be_empty
-        expect(e['code']).to be_a(Integer)
-        expect(e['timestamp']).to match(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}[A-Z]/)
-      end
+    it 'matches json schema' do
+      expect(subject).to match_response_schema('current_status')
     end
   end
 
@@ -25,15 +18,6 @@ describe DashboardController, type: :controller do
 
     it 'returns 200' do
       expect(subject.status).to eq(200)
-    end
-
-    it 'returns well formated json' do
-      json = JSON.parse(subject.body)
-      expect(json).to be_a(Hash)
-
-      expect(json.dig('results', 0, 'name')).not_to be_empty
-      expect(json.dig('results', 0, 'code')).to be_a(Integer)
-      expect(json.dig('results', 0, 'timestamp')).to match(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}[A-Z]/)
     end
 
     it 'matches json schema' do
