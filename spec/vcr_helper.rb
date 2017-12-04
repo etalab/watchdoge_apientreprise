0a97c0cc9021ec3432bb384010e3eb9e038481e5
Alexis Leclerc <pihaelle@gmail.com>
2017-12-04 14:42:51 +0100

    [VCR] math on uri, method AND body (doing)

--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -3,8 +3,9 @@ VCR.configure do |c|
   c.hook_into :webmock
   c.cassette_library_dir = 'spec/cassettes'
   c.configure_rspec_metadata!
-  c.default_cassette_options = { allow_playback_repeats: true }
-  # c.default_cassette_options = { allow_playback_repeats: true, record: :new_episodes }
+  c.default_cassette_options = { allow_playback_repeats: true, match_requests_on: [:method, :uri] } # TODO: add :body
+  # c.default_cassette_options = { record: :new_episodes, allow_playback_repeats: true, match_requests_on: [:method, :uri, :body] }
+
 
   c.filter_sensitive_data('<APIE_TOKEN>') { Rails.application.config_for(:secrets) ['apie_token'].to_s }
   c.filter_sensitive_data('<APIE_BASE_URI_NEW>') { Rails.application.config_for(:secrets) ['apie_base_uri_new'].to_s }
