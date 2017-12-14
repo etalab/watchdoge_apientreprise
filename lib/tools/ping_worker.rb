b76f0528809ec0260ade9fca7c1a0fbcdc33904e
Alexis Leclerc <pihaelle@gmail.com>
2017-12-14 17:54:52 +0100

    remove multi-threading

--- lib/tools/ping_worker.rb
+++ lib/tools/ping_worker.rb
@@ -1,17 +1,7 @@
 class Tools::PingWorker
   def run(elements, thread_number = Rails.application.config.thread_number)
-    @queue = elements.inject(Queue.new, :enq)
-
-    @threads = Array.new(thread_number) do
-      Thread.new do
-        until @queue.empty?
-          element = @queue.deq
-
-          yield element
-        end
-      end
+    elements.each do |e|
+      yield e
     end
-
-    @threads.each(&:join)
   end
 end
