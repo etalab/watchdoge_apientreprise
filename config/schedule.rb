8c2b93db6d270a1bfcff1ba65d2f17d2b472356f
Alexis Leclerc <alexis.leclerc@data.gouv.fr>
2018-01-18 19:15:41 +0200

    add performance infos about whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,12 @@
 # Learn more: http://github.com/javan/whenever
 set :output, File.join(Whenever.path, 'log', 'watchdoge_cron.log')
 
+# Perfo infos:
+# whenever can do the job of the rake task here
+# BUT it will start one Rails instance of each endpoint
+# so for every 60.minutes it will create 33 Rails instance
+# this way there is only one Rails instance started on each schedule
+
 # If you add a schedule make sure no endpoints are lost...
 # all endpoints currently belongs to period 1, 5 or 60
 every 1.minute do
