6d722ee9ade7065d39f1b2d8a275ba47e1094a77
Alexis Leclerc <alexis.leclerc@data.gouv.fr>
2018-03-21 16:27:57 +0200

    [RPSEC] fix routing

--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 describe 'routes', type: :routing do
   it 'route to current status' do
-    expect(get('dashboard/current_status')).to route_to(controller: 'dashboard', action: 'current_status')
+    expect(get('api/watchdoge/dashboard/current_status')).to route_to(controller: 'dashboard', action: 'current_status')
   end
 
   it 'route to history' do
-    expect(get('dashboard/availability_history')).to route_to(controller: 'dashboard', action: 'availability_history')
+    expect(get('api/watchdoge/dashboard/availability_history')).to route_to(controller: 'dashboard', action: 'availability_history')
   end
 
   it 'route to homepage status' do
-    expect(get('dashboard/homepage_status')).to route_to(controller: 'dashboard', action: 'homepage_status')
+    expect(get('api/watchdoge/dashboard/homepage_status')).to route_to(controller: 'dashboard', action: 'homepage_status')
   end
 end
