914e9467272b16f96cb36c76bd78d6400c04fde5
Alexis Leclerc <alexis.leclerc@data.gouv.fr>
2018-03-30 18:04:54 +0200

    [RSPEC] speed-up

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,6 +41,7 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.filter_run_excluding very_long_test: true
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
