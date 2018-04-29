84e3a584f3c1c5ba6b92b6d0a074ce7a39315e99
Alexis Leclerc <alexis.leclerc@data.gouv.fr>
2018-04-29 22:16:33 +0300

    Boyscout

--- spec/controllers/stats_controller_spec.rb
+++ spec/controllers/stats_controller_spec.rb
@@ -5,14 +5,14 @@ describe StatsController, type: :controller do
     subject { get :jwt_usage, params: { jti: jti } }
 
     describe 'happy path (e2e spec)', vcr: { cassette_name: 'stats/jwt_usage' } do
-      let(:jti) { valid_jti}
+      let(:jti) { valid_jti }
 
       its(:status) { is_expected.to eq(200) }
       its(:body) { is_expected.to match_json_schema('stats/jwt_usage') }
     end
 
     describe 'when having an error', vcr: { cassette_name: 'stats/jwt_usage' } do
-      let(:jti) { valid_jti}
+      let(:jti) { valid_jti }
 
       before { allow_any_instance_of(Stats::JwtUsageService).to receive(:success?).and_return(false) }
 
