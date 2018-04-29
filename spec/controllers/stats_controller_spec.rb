ea653575b10596364ff0177a8b8c35c66c5c96b1
Alexis Leclerc <alexis.leclerc@data.gouv.fr>
2018-04-29 21:40:30 +0300

    [STATS] wrong jti returns empty 200

--- spec/controllers/stats_controller_spec.rb
+++ spec/controllers/stats_controller_spec.rb
@@ -2,19 +2,28 @@ require 'rails_helper'
 
 describe StatsController, type: :controller do
   context 'with jwt_usage action' do
+    subject { get :jwt_usage, params: { jti: jti } }
+
     describe 'happy path (e2e spec)', vcr: { cassette_name: 'stats/jwt_usage' } do
-      subject { get :jwt_usage, params: { jti: valid_jti } }
+      let(:jti) { valid_jti}
 
       its(:status) { is_expected.to eq(200) }
       its(:body) { is_expected.to match_json_schema('stats/jwt_usage') }
     end
 
     describe 'when having an error', vcr: { cassette_name: 'stats/jwt_usage' } do
-      subject { get :jwt_usage, params: { jti: valid_jti } }
+      let(:jti) { valid_jti}
 
       before { allow_any_instance_of(Stats::JwtUsageService).to receive(:success?).and_return(false) }
 
       its(:status) { is_expected.to eq(500) }
+    end
+
+    describe 'when token returns 0 results', vcr: { cassette_name: 'stats/jwt_usage_empty_token' } do
+      let(:jti) { 'it_has_0_data' }
+
+      its(:status) { is_expected.to eq(200) }
+      its(:body) { is_expected.to match_json_schema('stats/jwt_usage') }
     end
   end
 
