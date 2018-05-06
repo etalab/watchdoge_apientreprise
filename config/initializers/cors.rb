9c41f401a8414e4e5ff7e13356a3b56c06f1ce23
Alexis Leclerc <alexis.leclerc@data.gouv.fr>
2018-05-06 13:01:31 +0300

    [CORS] enabled

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,20 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+
+    resource '/api/watchdoge/dashboard/*',
+      headers: :any,
+      methods: [:get, :head]
+  end
+
+  allow do
+    origins 'api.gouv.fr'
+
+    resource '/api/watchdoge/stats/last_30_days_usage',
+      headers: :any,
+      methods: [:get, :head]
+  end
+end
