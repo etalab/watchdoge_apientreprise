8faf9a9458e0ab299b4197a8ec7abe56c060b75a
Alexis Leclerc <alexis.leclerc@data.gouv.fr>
2018-05-30 17:40:28 +0300

    [CORS] deploy cors via ansible

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,7 +49,8 @@ set :shared_dirs, fetch(:shared_dirs, []).push(
 
 set :shared_files, fetch(:shared_files, []).push(
   'config/database.yml',
-  'config/secrets.yml'
+  'config/secrets.yml',
+  'config/initializers/cors.rb'
 )
 
 # This task is the environment that is loaded for all remote run commands, such as
