55baab2b21cd192018d6216b7601c97b9b8eeb94
Alexis Leclerc <alexis@alxs.fr>
2019-06-16 16:23:06 +0300

    Better test for cache

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -3,11 +3,21 @@ require 'rails_helper'
 describe 'Dashboard', type: :request do
   before { Rails.cache.clear }
 
-  example 'second response comes from cache', vcr: { cassette_name: 'dashboard/current_status' } do
-    expect_any_instance_of(Dashboard::CurrentStatusService)
-      .to receive(:perform).once.and_call_original
+  def current_cache
+    Rails.cache.read 'Dashboard::CurrentStatusService'
+  end
 
+  let(:expected_cache_value) do
+    {
+      success: true,
+      results: be_an(Array),
+      errors: ''
+    }
+  end
+
+  example 'cache is available after first call', vcr: { cassette_name: 'dashboard/current_status' } do
+    expect(current_cache).to be_nil
     get dashboard_current_status_path
-    get dashboard_current_status_path
+    expect(current_cache).to include(expected_cache_value)
   end
 end
