0da16c2632163b7ca9151607b86c65d1ca3412f7
Alexis Leclerc <alexis@alxs.fr>
2019-06-18 15:21:38 +0300

    Rails cache better specs

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Dashboard', type: :request do
+describe 'Dashboard', type: :request, vcr: { cassette_name: 'dashboard/current_status' } do
   before { Rails.cache.clear }
 
   def current_cache
@@ -15,9 +15,22 @@ describe 'Dashboard', type: :request do
     }
   end
 
-  example 'cache is available after first call', vcr: { cassette_name: 'dashboard/current_status' } do
+  example 'cache is available after first call' do
     expect(current_cache).to be_nil
     get dashboard_current_status_path
     expect(current_cache).to include(expected_cache_value)
+  end
+
+  example 'cache is returned at the second call because service if not called twice' do
+    expect_any_instance_of(Dashboard::CurrentStatusService)
+      .to receive(:perform).once.and_call_original
+    get dashboard_current_status_path
+    get dashboard_current_status_path
+  end
+
+  example 'cache expires after 5 minutes' do
+    get dashboard_current_status_path
+    Timecop.travel Time.zone.now + 6.minutes
+    expect(current_cache).to be_nil
   end
 end
